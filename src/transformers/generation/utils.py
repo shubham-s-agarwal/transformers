4c4e04cf48360e35d259c28dfc7b546d3c3f1906
Shubham Agarwal <66172189+shubham-s-agarwal@users.noreply.github.com>
2025-04-25 15:09:50 +0100

    Update utils.py

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -2357,11 +2357,12 @@ class GenerationMixin:
             and not self.config.is_encoder_decoder
         ):
             max_cache_length += inputs_tensor.shape[1]
-        
+        print("MODEL KWARGS BEFORE",model_kwargs)
         self._prepare_cache_for_generation(
             generation_config, model_kwargs, assistant_model, batch_size, max_cache_length, device
         )
         print("MODEL KWARGS",model_kwargs)
+        print(len(model_kwargs['past_key_values'].key_cache))
         # 8. determine generation mode
         generation_mode = generation_config.get_generation_mode(assistant_model)
 
